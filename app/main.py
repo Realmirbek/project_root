0d5b3735b77ada1bd2ef61a0573ee18c2227490b
mirbek <mamytalievmirbek2008@gmail.com>
2025-06-23 16:55:47 +0600

    new

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from app.routers import pages
-from app.routers import contact
+from app.routers import pages, contact
+
 
 
 app = FastAPI()
@@ -15,6 +15,8 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 # <link rel="stylesheet" href="/static/css/style.css">
+
+# yodf bcle ihli mpkz
 
 
 
